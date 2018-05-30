ad337d2863aadd3a682e39b321cd052bf0f1307e
宋浩茹 <eerica841205@gmail.com>
2018-05-30 23:59:32 +0800

    add file

---
+++ 買雞/買雞/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 買雞
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0.0;
+            double y = 0.0;
+            double z = 0.0;
+
+            while(((x + y + z) != 100.0) && ((60*x) + (30*y) + (z)) != 1000.0)
+            {
+                x++;
+                if(((900.0 - (59.0 * x)) % 29.0) == 0.0)
+                {
+                    y = (((900.0) - (59.0 * x)) / 29.0);
+                    z = (100.0 - x - y);
+                }
+            }
+            Console.WriteLine($"公雞 : {x} 母雞 : {y} 小雞 : {z}");
+            Console.ReadLine();
+        }
+    }
+}
